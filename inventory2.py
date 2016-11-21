7331149be5c084d79ae7674e8393143a16039fb6
mikeal888 <michael.kewming@gmail.com>
2016-11-22 08:20:13 +1100

    inventory2.py dicking around, learning requests is pretty obvious.

--- inventory2.py
+++ inventory2.py
@@ -1,1 +1,19 @@
 # Start a new inventory but make make inventory class and stock class
+
+class stock:
+	# Create a stock object 
+	def __init__(self, name, price, quantity):
+		self.name = name				# Name of item
+		self.price = price				# Total price of items
+		self.quantity = quantity		# Total quantity of items
+
+
+class inventory:
+	# create inventory object
+	def __init__(self, name, stock):
+		self.name = name 				# Name of inventory
+		self.stock = {} 				# Stock in inventory
+
+	def add(self, ITEM):
+		# Add stock to inventory
+		self.stock[ITEM.name] = ITEM
