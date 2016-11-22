42a7f0eec569f3583177dcf45938b6502dc23875
mikeal888 <michael.kewming@gmail.com>
2016-11-22 21:57:38 +1100

    Inventory2 now includes dictionary object of inventory

--- inventory2.py
+++ inventory2.py
@@ -1,4 +1,5 @@
 # Start a new inventory but make make inventory class and stock class
+# Inventory must take stock objects 
 
 class stock:
 	# Create a stock object 
@@ -8,12 +9,39 @@ class stock:
 		self.quantity = quantity		# Total quantity of items
 
 
-class inventory:
-	# create inventory object
-	def __init__(self, name, stock):
-		self.name = name 				# Name of inventory
-		self.stock = {} 				# Stock in inventory
+class inventory(dict):
+	# create a dictionary object.
+	# This does no require any initialisation.
+	# Only these dictionary objects will inherit the methods defined below
 
-	def add(self, ITEM):
-		# Add stock to inventory
-		self.stock[ITEM.name] = ITEM
+	def add(self, id, val):
+		
+		# Add contents to inventory
+
+		if id in self.keys():
+			self[id] += val
+			print('Added', val, 'units of', id)
+			print('Total units of', id, '=', self[id])
+		else:
+			self[id] = val
+			print('Added', val, 'units of', id)
+
+	def remove(self, id, val):
+
+		# Remove contents from inventory
+		
+		if id in self.keys():
+			self[id] -= val
+			print('Removed', val, 'units of', id)
+			print('Total units of', id, '=', self[id])
+		else:
+			print(id, 'not in inventory')
+
+	def items(self):
+
+		# Print items in inventory
+		
+		print('item', 'units')
+		print()
+		for index in self.keys():
+			print(index, self[index])	
